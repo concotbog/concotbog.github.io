84d29ce3f2861450bd29c56a40cb7ffb0980b370
Marian-Hristov <marianhr2014@gmail.com>
2026-05-18 12:48:01 -0400

    Deploy production build to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/concotbog.github.io/",
+  base: "./",
   plugins: [react()],
-})
+  build: {
+    outDir: ".", 
+    emptyOutDir: false,
+  },
+});
