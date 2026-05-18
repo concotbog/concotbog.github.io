de7fb9ea81a51e3efee3003564abac6426e80036
Marian-Hristov <marianhr2014@gmail.com>
2026-05-18 13:41:40 -0400

    Add deploy instructions + slight image compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/concotbog.github.io/",
-  plugins: [react()],
+  base: "/",
+  plugins: [
+    react(),
+    viteImagemin({
+      optipng: {
+        optimizationLevel: 7,
+      },
+      jpegTran: {
+        progressive: true,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+            active: false,
+          },
+        ],
+      },
+      gifsicle: {
+        optimizationLevel: 3,
+      },
+    }),
+  ],
   build: {
     outDir: "dist",
     emptyOutDir: false,
